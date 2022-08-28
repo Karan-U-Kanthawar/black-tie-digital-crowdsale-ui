d5a5918795e23081f00bd80bc614a35a0def85f3
Karan <karankanthawar@gmail.com>
2022-08-28 10:01:21 +0530

    feat: Add bg color black

--- src/views/Presale/index.tsx
+++ src/views/Presale/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import IVCOPage from "./IVCOPage";
 import { Container } from "@mui/material";
 import { BLACK_TIE_DIGITAL_PRESALE_ID } from "../../config";
+import styled from "styled-components";
+
+const BlackContainer = styled.div`
+  background-color: #000;
+`;
 
 const Presale: React.FC = () => {
   return (
-    <Container>
-      <IVCOPage id={BLACK_TIE_DIGITAL_PRESALE_ID} />
-    </Container>
+    <BlackContainer>
+      <Container>
+        <IVCOPage id={BLACK_TIE_DIGITAL_PRESALE_ID} />
+      </Container>
+    </BlackContainer>
   );
 };
 
